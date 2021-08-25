5403f4f53d3c6d86fdb311ddb3e85f39a4ae3d62
Mandeep Kaur <Mandeep.Kaur@BDINDIA.LOCAL>
2021-08-25 13:32:22 +0530

    fixed liveness and readiness in both services

--- serviceA/src/A.py
+++ serviceA/src/A.py
@@ -20,6 +20,10 @@ def internal_error(error):
     return "500 error - Internal Server Exception", 500
 
 
+@app.route('/health')
+def hello_world():
+    return "I am healthy"
+
 #if __name__ == '__main__':
 #    app.run('0.0.0.0',5001)
 
